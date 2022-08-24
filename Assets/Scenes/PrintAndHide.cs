1bee199b8382acd3df0c5164dc5adbad74dedab6
Liangyi Wang <sun133233@gmail.com>
2022-08-24 18:05:35 +0800

    100 percent complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -6,11 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    public int randomInteger;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomInteger = Random.Range(200,251);
     }
 
     // Update is called once per frame
@@ -18,5 +20,9 @@ public class PrintAndHide : MonoBehaviour
     {
         int b = ++i;
         Debug.Log(gameObject.name +":" + b);
+        if (gameObject.CompareTag("Red") && b == 100)
+            gameObject.SetActive(false);
+        if (gameObject.CompareTag("Blue") && b == randomInteger)
+            rend.enabled = false;
     }
 }
